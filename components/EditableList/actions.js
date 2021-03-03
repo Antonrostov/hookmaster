2e034153c8821ee03112b4acffed09f3e7dbd73f
Anton <rostov.develop.ua@gmail.com>
2021-03-03 04:43:30 +0000

    Use custom hooks to refactor actions

--- components/EditableList/actions.js
+++ components/EditableList/actions.js
@@ -1,43 +1,50 @@
-export const add = async (text, state) => {
-  const items = state.agenda.items.slice();
-  items.push({
-    text
-  });
-  const data = await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const roll = Math.random();
-      if (roll < 0.5) {
-        resolve({
-          ...state,
-          agenda: {
-            items
-          }
-        });
-      } else {
-        reject(new Error("Failed to add item"));
-      }
-    }, 1000);
-  });
-  return data;
+import { useAppState } from "components/StateProvider";
+export const useAdd = () => {
+  const [state, setState] = useAppState();
+  return async text => {
+    const items = state.agenda.items.slice();
+    items.push({
+      text
+    });
+    const data = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const roll = Math.random();
+        if (roll < 0.5) {
+          resolve({
+            ...state,
+            agenda: {
+              items
+            }
+          });
+        } else {
+          reject(new Error("Failed to add item"));
+        }
+      }, 100);
+    });
+    return setState(data);
+  };
 };
-export const remove = async (item, state) => {
-  const data = await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const roll = Math.random();
-      if (roll < 0.5) {
-        const items = state.agenda.items.filter(i => {
-          return i !== item;
-        });
-        resolve({
-          ...state,
-          agenda: {
-            items
-          }
-        });
-      } else {
-        reject(new Error("Failed to delete item"));
-      }
-    }, 1000);
-  });
-  return data;
+export const useRemove = () => {
+  const [state, setState] = useAppState();
+  return async item => {
+    const data = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const roll = Math.random();
+        if (roll < 0.5) {
+          const items = state.agenda.items.filter(i => {
+            return i !== item;
+          });
+          resolve({
+            ...state,
+            agenda: {
+              items
+            }
+          });
+        } else {
+          reject(new Error("Failed to delete item"));
+        }
+      }, 100);
+    });
+    return setState(data);
+  };
 };
