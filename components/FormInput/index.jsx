ee0c5cd43403786dd0550a3c8d1334dcd4dc7417
Anton <rostov.develop.ua@gmail.com>
2021-03-20 04:11:09 +0000

    Add magic form component with custom hooks

---
+++ components/FormInput/index.jsx
@@ -0,0 +1,5 @@
+import { useChangeHandler } from "../Form/FormUtils";
+export default function FormInput(props) {
+  const { onChange, value } = useChangeHandler(props);
+  return <input {...props} onChange={onChange} value={value} />;
+}
