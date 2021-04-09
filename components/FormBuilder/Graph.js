ca5bfae5b9b7dc089945cf828b44ded6a80d199d
Anton <rostov.develop.ua@gmail.com>
2021-04-09 07:40:50 +0000

    Add component config into graph

--- components/FormBuilder/Graph.js
+++ components/FormBuilder/Graph.js
@@ -1,10 +1,11 @@
-import FormInput from "components/FormInput";
-import FormCheckbox from "components/FormCheckbox";
-import FormRadio from "components/FormRadio";
 function Node(data) {
   this.data = data;
   this.out = new Map();
 }
+Node.prototype.render = function() {
+  const Component = this.data.component;
+  return <Component {...this.data.props} />;
+};
 Node.prototype.to = function(node, condition) {
   this.out.set(node, condition);
 };
@@ -20,18 +21,10 @@ Graph.prototype.runChanges = function({ oldState, newState, changedNodes }) {
       newState = condition(oldState, newState);
     });
   });
-  console.log(newState);
-  const components = (
-    <div>
-      <FormInput name="one" validate="number" />
-      <FormInput name="two" validate="number" />
-      <FormCheckbox name="check_1" />
-      <FormCheckbox name="check_2" />
-      <FormCheckbox name="check_3" />
-      <FormRadio id="radio_female" name="radio_1" value="female" />
-      <FormRadio id="radio_male" name="radio_1" value="male" />
-    </div>
-  );
+  const components = Object.keys(newState).map(name => {
+    const node = this.nodes[name];
+    return node.render();
+  });
   return [newState, components];
 };
 export default Graph;
