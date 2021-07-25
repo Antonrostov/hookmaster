be4af7ef0abb9ffb62e8901b71178ba943694b3c
Anton <rostov.develop.ua@gmail.com>
2021-07-25 02:22:30 +0000

    Fix errors not showing on submit

--- components/Form/FormUtils.jsx
+++ components/Form/FormUtils.jsx
@@ -87,6 +87,9 @@ function useStoreStrategy(opts) {
   }
   const state = store.data[name];
   const error = getError(validate, state);
+  if (isInitialMount.current && error !== null) {
+    store.errors[name] = error;
+  }
   useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
