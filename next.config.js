7eaffd7c881d26af0f39165aba972d021efb588a
Anton <rostov.develop.ua@gmail.com>
2021-10-03 16:00:32 +0000

    Update deployment config

--- next.config.js
+++ next.config.js
@@ -13,8 +13,7 @@ const plugins = [
   ],
   [withFonts]
 ];
-config = withPlugins([...plugins]);
-if (process.env.DEPLOY) {
-  config.target = "serverless";
-}
+config = withPlugins([...plugins], {
+  target: "serverless"
+});
 module.exports = config;
