a14c16d0ddb9fcfd42edb556bb58e6d75aaf9263
Anton <rostov.develop.ua@gmail.com>
2021-12-14 00:36:43 +0000

    Handle initial state setting

--- components/FormBuilder/index.jsx
+++ components/FormBuilder/index.jsx
@@ -2,20 +2,14 @@ import Form from "components/Form";
 import Button from "components/Button";
 import { useGraph } from "components/FormBuilder/GraphUtils";
 export default function FormBuilder(props) {
-  const { config, initialState, ...other } = props;
+  const { config, onSubmit, ...other } = props;
   const { ui, runChanges } = useGraph(config);
-  function onSubmit(state, errors) {
-    console.log("form", state);
-    console.log("errors", errors);
-    alert("Check the developer console!");
-  }
   return (
     <Form
       onChange={(newState, changes) => {
         runChanges(changes);
       }}
       onSubmit={onSubmit}
-      data={initialState}
       {...other}
     >
       {ui}
